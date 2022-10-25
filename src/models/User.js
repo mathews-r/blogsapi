7f04de6806fa535fa1f9cd2f2d25ef82158f16d6
Mathews <mathewsrodrigues9@gmail.com>
2022-10-25 16:06:53 -0300

    requisito 2 concluido

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+      id: DataTypes.INTEGER,
+      display_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+      {
+        tableName: "users",
+        timestamps: false,
+      }
+      );
+  return User;
+};
