828c47c93193bdaf849681cf61b6d868b4c19a73
Mathews <mathewsrodrigues9@gmail.com>
2022-10-25 19:28:15 -0300

    requisito 3 ok

--- src/middlewares/loginValidation.js
+++ src/middlewares/loginValidation.js
@@ -1,11 +1,8 @@
 const loginValidation = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    res.status(400).json({ message: 'Some required fields are missing' });
-  }
-  if (email.length === 0 || password.length === 0) {
-    res.status(400).json({ message: 'Some required fields are missing' });
+  if (!email || !password || email.length === 0 || password.length === 0) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
   }
 
   return next();
