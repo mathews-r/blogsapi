91d99114b757a0addca054f905c855af0bf0e033
Mathews <mathewsrodrigues9@gmail.com>
2022-10-26 12:37:19 -0300

    requisito 7 concluido

---
+++ src/models/Category.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataTypes.STRING,
+    }
+  )
+  return Category;
+}
